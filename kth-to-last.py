0349267feb09781d276e0cb648ae06169ed80dfb
Austin Hendricks <hendraustin@gmail.com>
2024-05-29 15:27:12 -0700

    Add LinkedList and Node classes with simple LL object

---
+++ kth-to-last.py
@@ -0,0 +1,51 @@
+# Return kth-to-last: Implement algo to find the kth to last element of a singly linked list
+class LinkedListNode:
+
+    def __init__(self, value, nextNode=None, prevNode=None):
+        self.value = value
+        self.next = nextNode
+        self.prev = prevNode
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+
+    def __str__(self):
+        values = [str(x) for x in self]
+        return " -> ".join(values)
+
+    def __len__(self):
+        result = 0
+        node = self.head
+        while node:
+            result += 1
+            node = node.next
+        return result
+
+    def add(self, value):
+        if self.head is None:
+            self.tail = self.head = LinkedListNode(value)
+        else:
+            self.tail.next = LinkedListNode(value)
+            self.tail = self.tail.next
+        return self.tail
+
+
+llist = LinkedList()
+llist.add(2)
+llist.add(5)
+llist.add(7)
+llist.add(12)
+llist.add(9)
